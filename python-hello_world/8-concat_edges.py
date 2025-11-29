09ed97600b387bee62e05b2e896fd83f5e779483
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-11-29 15:01:27 +0000

    s

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# Reassign str to the concatenated slice to match the desired output
 str = str[39:67] + "with " + str[0:6]
 print(str)
