0af7c12fad56a4c857e9ee574f0cc83f6675b928
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-11-29 17:52:20 +0000

    aa

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
-
 def reverse():
-
     i = 0
     for char_code in range(ord('z'), ord('a') - 1, -1):
-
         if i % 2 != 0:
-
-            char_code-= 32
-
+            char_code -= 32
         print("{}".format(chr(char_code)), end="")
         i += 1
+
+
 reverse()
