52b35253d6a34484ede1c050d2a20197733dde90
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-11-29 17:45:55 +0000

    a

---
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def reveresengineeringlol():
+
+    i = 0
+    for char_code in range(ord('z'), ord('a') - 1, -1):
+
+        if i % 2 != 0:
+
+            char_code-= 32
+
+        print("{}".format(chr(char_code)), end="")
+        i += 1
+    print()
+reverseengineeringlol()
