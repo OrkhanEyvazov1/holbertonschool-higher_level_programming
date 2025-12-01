f08208a2a34baefbbf3f10652e40d3339718d264
OrkhanEyvazov1 <orkhan50616@sabah.edu.az>
2025-12-01 09:59:33 +0000

    ok

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,22 +4,22 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    print("{} + {} ={} ".format(
+    print("{} + {} = {}".format(
         a,
         b,
         add(a, b)
     ))
-    print("{} - {} ={} ".format(
+    print("{} - {} = {}".format(
         a,
         b,
         sub(a, b)
     ))
-    print("{} * {} ={} ".format(
+    print("{} * {} = {}".format(
         a,
         b,
         mul(a, b)
     ))
-    print("{} / {} ={} ".format(
+    print("{} / {} = {}".format(
         a,
         b,
         div(a, b)
