b891cb0748138c283813f84c81c931dfe94bd07c
OrkhanEyvazov1 <orkhan50616@sabah.edu.az>
2025-12-01 10:49:31 +0000

    done

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+import sys
+
+
+def main():
+    total_sum = 0
+    for arg in sys.argv[1:]:
+        
+        total_sum += int(arg)
+            
+    print(total_sum)
+
+
+if __name__ == "__main__":
+    main()
