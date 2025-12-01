918ee458bed08fd787d76ff4b49e28cd7637c06e
OrkhanEyvazov1 <orkhan50616@sabah.edu.az>
2025-12-01 10:35:05 +0000

    a

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
-
 import sys
-
-
 def main():
     arg_count = len(sys.argv) - 1
-
     if arg_count == 0:
         print("0 arguments.")
     else:
-        
         plural = "s" if arg_count > 1 else ""
         print(f"{arg_count} argument{plural}:")
         for i, arg in enumerate(sys.argv[1:], 1):
-
             print(f"{i}: {arg}")
-
-
 if __name__ == "__main__":
     main()
