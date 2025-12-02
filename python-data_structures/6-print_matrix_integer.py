bb6c319914872d28ab32ef8d7dc8f2a54894286e
OrkhanEyvazov1 <orhane50616@sabah.edu.az>
2025-12-02 11:21:04 +0000

    a

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def print_matrix_integer(matrix = [[]]):
+def print_matrix_integer(matrix=[[]]):
     for list_ in matrix:
         for i, e in enumerate(list_):
             print("{:d}".format(e), end=' ' if i != len(list_) - 1 else '')
