bac38bb90f866e360b523428b1cf227ef4954ad6
OrkhanEyvazov1 <orhane50616@sabah.edu.az>
2025-12-02 11:59:53 +0000

    aa

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,9 +3,11 @@
 def multiple_returns(sentence):
     length = len(sentence)
 
-    if length==0:
+    if length == 0:
         first = None
-        return print("Length: {:d} - First character: {}".format(length, first))
+        return print(
+        "Length: {:d} - First character: {}".format(length, first))
     else:
         first = sentence[0]
-        return print("Length: {:d} - First character: {}".format(length, first))
+        return print(
+        "Length: {:d} - First character: {}".format(length, first))
