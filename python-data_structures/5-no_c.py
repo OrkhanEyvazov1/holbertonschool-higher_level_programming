673a444eb62ddf54a6b2fca824912a6acd8c3830
OrkhanEyvazov1 <orhane50616@sabah.edu.az>
2025-12-02 10:55:39 +0000

    a

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    for char in my_string:
-        if char == "c" or char == "C":
-            print("")
-        else:
-            print(char,end = "")
+    return ''.join(map(
+        lambda x: '' if x == 'C' or x == 'c' else x, my_string
+    ))
