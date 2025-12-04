a375eed6aee905ca0c5272f343f50a79ad38cc19
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-04 12:57:03 +0000

    9-multiply_by_2.py

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    for a in a_dictionary.keys():
-        a_dictionary[a] = a_dictionary[a]*2
-    return a_dictionary
+    new_dict = {}
+    for a, b in a_dictionary.items():
+        new_dict[a] = b * 2
+    return new_dict
