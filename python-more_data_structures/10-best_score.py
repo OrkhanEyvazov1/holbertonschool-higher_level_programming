d2300c23f54662ddb816e7d97aeb28bbe106599a
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-04 13:21:17 +0000

    a

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,14 +4,11 @@ def best_score(a_dictionary):
 
     if not a_dictionary:
         return None
-    
     first_key = next(iter(a_dictionary))
     best_key = first_key
-    max_score = a_dictionary[first_key]
-    
+    max_score = a_dictionary[first_key]    
     for k, w in a_dictionary.items():
         if w > max_score:
             max_score = w
             best_key = k
-            
     return best_key
