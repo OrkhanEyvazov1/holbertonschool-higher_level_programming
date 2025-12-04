f72b3b359eca8c86e366c9827e17f3d9b2273eec
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-04 14:47:47 +0000

    a

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
-    roman = { "I" : 1, "V" : 5, "X" : 10, "C" : 100, "D" : 500, "M" : 1000}
+    roman = { "I" : 1, "V" : 5, "X" : 10, "C" : 100,"L" : 50, "D" : 500, "M" : 1000}
     sum = 0
     for i in range(len(roman_string)-1):
             curr = roman[roman_string[i]]
