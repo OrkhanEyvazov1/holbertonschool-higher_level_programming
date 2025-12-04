58f7824e6abc7be46ff06e4c5a226090110d762c
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-04 13:17:43 +0000

    10-best_score.py

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+
+    if not a_dictionary:
+        return None
+    
+    first_key = next(iter(a_dictionary))
+    best_key = first_key
+    max_score = a_dictionary[first_key]
+    
+    for k, w in a_dictionary.items():
+        if w > max_score:
+            max_score = w
+            best_key = k
+            
+    return best_key
