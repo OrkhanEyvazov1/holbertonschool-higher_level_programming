75461c0183a71b51decd4ab440c1dd996733ee9d
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-04 14:46:18 +0000

    a

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string)0:
+def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
     roman = { "I" : 1, "V" : 5, "X" : 10, "C" : 100, "D" : 500, "M" : 1000}
