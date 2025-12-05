93855066e29ff234f228c9ee557da1175f438b29
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-05 13:48:39 +0000

    a

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 printed_count = 0
-    for i in range(x):
-        try:
-            print(my_list[i], end="")
-            printed_count += 1
-        except IndexError:
-            break
-    return printed_count
+for i in range(x):
+    try:
+        print(my_list[i], end="")
+        printed_count += 1
+    except IndexError:
+        break
+return printed_count
