e79c353191dd85083e7d83dae71a70d7de47abe6
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-09 11:06:52 +0000

    a

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -24,7 +24,8 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value    
+        self.__width = value
+    
     @property
     def height(self):
         '''ret height of reqtangle'''
