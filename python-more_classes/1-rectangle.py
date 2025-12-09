7ce1474d7c256f48e3f61f8920048cde54199dcc
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-09 10:56:57 +0000

    rectangle width height

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''
+doc here
+'''
+
+
+class Rectangle:
+    '''Defy square. '''
+
+    def __init__(self, width=0,height=0):
+        '''Init square'''
+        self.width = width
+
+    @property
+    def width(self):
+        '''ret width of reqtangle'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''set the width of reqtangle'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__width = value
+    
+    @property
+    def height(self):
+        '''ret height of reqtangle'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''set the height of reqtangle'''
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__height = value
