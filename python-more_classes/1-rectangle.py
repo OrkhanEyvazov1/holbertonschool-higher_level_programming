19679a89557534abc6013ddedcbb1360622edfab
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-09 11:10:08 +0000

    a

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,39 +1,50 @@
-#!/usr/bin/python3
-'''
-doc here
-'''
+#!/usr/bin/env python3
+"""
+Module documentation goes here.
+"""
 
 
 class Rectangle:
-    '''Defy square. '''
+    """Define a rectangle.
+    
+    (Note: The original docstring said 'Defy square', which is also
+    inconsistent with the class name.)
+    """
 
     def __init__(self, width=0, height=0):
-        '''Init square'''
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
         self.width = width
         self.height = height
 
+
     @property
     def width(self):
-        '''ret width of reqtangle'''
+        """Retrieve the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''set the width of reqtangle'''
+        """Set the width of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
+
     @property
     def height(self):
-        '''ret height of reqtangle'''
+        """Retrieve the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''set the height of reqtangle'''
+        """Set the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
