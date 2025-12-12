2fe5db159f93489294ff49e62cb12e680b47d3cc
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-12 13:25:50 +0000

    a

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+doc here
+'''
+
+
+class BaseGeometry():
+    '''
+    raise error
+    '''
+    def area(self):
+        '''
+        raise excep
+        '''
+        raise Exception("area() is not implemented")
+
