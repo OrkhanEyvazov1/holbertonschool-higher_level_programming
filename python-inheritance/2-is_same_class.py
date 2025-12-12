5f50b253a93505e6ecf8a832ab689a47ac58c73a
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-12 12:46:35 +0000

    a

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+doc here
+'''
+
+
+def is_same_class(obj, a_class):
+    ''' return true if obj is instance of exact class '''
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
