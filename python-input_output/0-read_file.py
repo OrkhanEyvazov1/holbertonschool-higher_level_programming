919f095a82c0240c4f5c3770bc65c086f7886b7e
OrkhanEyvazov1 <orkhane60616@sabah.edu.az>
2025-12-13 15:12:52 +0000

    io

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+io file read
+'''
+
+
+def read_file(filename=""):
+    """Function to read and print file"""
+    with open("{}".format(filename), encoding="utf-8") as f:
+        print(f.read(), end="")
