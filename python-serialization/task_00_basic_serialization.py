7eb084fb673fa38abb990ef2e0095b2a5cb47759
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-12-15 09:13:18 +0000

    encoding

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -6,10 +6,11 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     '''serialize data to json'''
-    with open(filename, 'w') as file:
-        json.dump(data, file)
+    with open(filename, 'w', encoding='utf-8') as f:
+            return json.dump(data, f)
+
 
 def load_and_deserialize(filename):
     '''deserialize json'''
-    with open(filename, 'r') as file:
-        json.load(file)
+    with open(filename, 'r', encoding='utf-8') as f:
+            return json.load(f)
